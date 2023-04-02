d3025c972fa7ec42e0f612a2315d7f2728b18b97
IzansStore <115550578+IzansStore@users.noreply.github.com>
2023-04-02 09:01:46 +0700

    Update settings.js
    Y

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
-global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.namabot = "ZaaBot"
+global.namaowner = "Faza"
+global.packname = Zaabot
+global.author = Faza
+global.myweb = "https://s.id/Izans"
+global.footer_text = "© " + Zaabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6282298627146']
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 100kb agar welcome nya gk lemot
 global.prefa = ['-_-']
